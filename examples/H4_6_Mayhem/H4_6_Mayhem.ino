8f9aedef8c4d08a61ad9f7dcbec3312899f85355
Phil Bowles <philbowles2012@gmail.com>
2017-12-31 17:25:31 +0100

    v0.9.3
    when/whenever added

---
+++ examples/H4_6_Mayhem/H4_6_Mayhem.ino
@@ -0,0 +1,95 @@
+/*
+ MIT License
+
+Copyright (c) 2017 Phil Bowles
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ *    Demonstrates usage of the H4 timer library with: 
+ *      - randomTimes function calls function at fixed interval, but only a random number of times
+ *      - randomTimesRandom function calls function a random number of times at random intervals
+ *      
+ *      
+ *      NB watch H4_3_Chaining first to see how it works
+ */
+#include <H4.h>
+
+H4  h4;
+
+class simpleClass{
+  public:
+    void print(const char* x){
+      Serial.printf("T=%d simpleClass: %s\n",millis(),x);  
+    }
+};
+
+simpleClass sC;
+
+void simpleFunction(uint32_t* x){
+  Serial.printf("T=%d It's easy as %d\n",millis(),(*x)++);
+}
+
+void toggleBuiltin(){
+    digitalWrite(LED_BUILTIN,!digitalRead(LED_BUILTIN));
+}
+
+void setup() {
+  static uint32_t simple;
+  static uint32_t counter;
+  
+  Serial.begin(74880);
+  pinMode(LED_BUILTIN,OUTPUT);
+  h4.every(250,toggleBuiltin);                                          // flash LED rapidly on/off every 250 ms (4x per second)
+
+  H4_STD_FN  jackson5=[](){
+    Serial.print("The Jackson 5 sang: ");
+    h4.nTimes(3,1500,[]()
+      {
+      static char c=0x41;
+      Serial.printf("%c ",c++);       // run 3 times...A B C
+      },
+      []()                            // and then... chain function
+        {
+        Serial.print("\nIt's easy as: ");
+        h4.nTimes(3,1500,[](){
+          static int n=1;
+          Serial.printf("%d ",n++);     // 1 2 3
+          },                          // and then... chain function
+          []()
+            {
+            Serial.print("\n...That's how easy it can be!\n");
+            h4.once(5000,[](){ Serial.println("have a listen to the real thing: https://www.youtube.com/watch?v=ho7796-au8U"); });
+            } // end 123 chain function
+        ); // end 123 function
+        } // end abc chain function
+      ); // end "ABC" function
+  }; // end fn declaration
+//  sing it...some time in the future
+  h4.randomTimes(1,5,30000,jackson5); // do it between 1 and 5 times (at least 30 seconds apart
+  h4.randomTimesRandom(2,4,15000,35000,jackson5); // this is certain to overlap with the previous causing mayhem
+}
+
+void loop() {
+  h4.loop();                                                           // you MUST call this as often as possible
+  if(!(millis()%5000)) {
+    Serial.printf("T=%d doing some stuff in main loop\n",millis());
+  }
+}
